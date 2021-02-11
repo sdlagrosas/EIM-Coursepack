afe1d61ab4a02e97ddf9e79a47c3f4611c520714
sdlagrosas <sdlagrosas@up.edu.ph>
2021-02-11 17:40:45 +0800

    Change text in main menu fragment and add Up button in the app bar

--- app/src/main/java/com/example/eim_coursepack/MainActivity.kt
+++ app/src/main/java/com/example/eim_coursepack/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.eim_coursepack
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.eim_coursepack.databinding.ActivityMainBinding
+
+private lateinit var binding : ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.navHostFragment)
+
+        NavigationUI.setupActionBarWithNavController(this,navController)
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.navHostFragment)
+        return navController.navigateUp()
     }
 }
