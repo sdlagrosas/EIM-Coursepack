a366c0b5207d3721320fd819d9c0616628cfc2b0
RodVG <golpechua@gmail.com>
2021-02-16 18:17:28 +0800

    Layout Continuation. Added logic, lesson and layout until UNIT 6

--- app/src/main/java/com/example/eim_coursepack/Unit5Fragment.kt
+++ app/src/main/java/com/example/eim_coursepack/Unit5Fragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.eim_coursepack.databinding.FragmentUnit5Binding
 
 
@@ -16,7 +17,15 @@ class Unit5Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = DataBindingUtil.inflate<FragmentUnit5Binding>(inflater,R.layout.fragment_unit5,container,false)
-
+        binding.unit5Lesson1Button.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_unit5Fragment_to_unit5Lesson1)
+        }
+        binding.unit5Lesson2Button.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_unit5Fragment_to_unit5Lesson2)
+        }
+        binding.unit5Lesson3Button.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_unit5Fragment_to_unit5Lesson3)
+        }
         return binding.root
     }
 
--- app/src/main/java/com/example/eim_coursepack/Unit6.kt
+++ app/src/main/java/com/example/eim_coursepack/Unit6.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.eim_coursepack.databinding.FragmentUnit6Binding
 
 
@@ -17,7 +18,18 @@ class Unit6 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = DataBindingUtil.inflate<FragmentUnit6Binding>(inflater,R.layout.fragment_unit6,container,false)
-
+        binding.unit6Lesson1Button.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_unit6_to_unit6Lesson1)
+        }
+        binding.unit6Lesson2Button.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_unit6_to_unit6Lesson2)
+        }
+        binding.unit6Lesson3Button.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_unit6_to_unit6Lesson3)
+        }
+        binding.unit6Lesson4Button.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_unit6_to_unit6Lesson4)
+        }
         return binding.root
     }
 
