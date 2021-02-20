cb9269b26b06d4f676a8e6b462432e6e8c694d2b
sdlagrosas <sdlagrosas@up.edu.ph>
2021-02-20 19:28:57 +0800

    Implement scoring logic and saving data in quiz 1

--- app/src/main/java/com/example/eim_coursepack/Unit1Quiz1MCFragment.kt
+++ app/src/main/java/com/example/eim_coursepack/Unit1Quiz1MCFragment.kt
@@ -1,5 +1,6 @@
 package com.example.eim_coursepack
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.opengl.Visibility
 import android.os.Bundle
@@ -61,30 +62,29 @@ class Unit1Quiz1MCFragment : Fragment() {
         ),
         Question(
             text = "11. It contains the positive and neutral charged of an atom.",
-            answers = listOf("nucleus","proton","electron","neutron"),
+            answers = listOf("nucleus"),
             isCorrect = false
         ),
         Question(
             text = "12. This refers to the attraction between the nucleus and the electron.",
-            answers = listOf("electrostatic force", "electricity", "current flow", "voltage"),
+            answers = listOf("electrostatic force"),
             isCorrect = false
         ),
         Question(
             text = "13. The particle which has equal charged to electron",
-            answers = listOf("proton", "neutron", "atom", "boson"),
+            answers = listOf("proton"),
             isCorrect = false
         ),
         Question(
             text = "14. Particle which found in the outer orbit of an atom.",
-            answers = listOf("electron", "proton", "neutron", "neutrino"),
+            answers = listOf("electron"),
             isCorrect = false
         ),
         Question(
             text = "15. Particle of an atom without electrical charge.",
-            answers = listOf("neutron", "neutrino", "proton", "electron"),
+            answers = listOf("neutron"),
             isCorrect = false
         )
-
     )
 
     lateinit var currentQuestion: Question
@@ -93,35 +93,43 @@ class Unit1Quiz1MCFragment : Fragment() {
     private val numQuestions = questions.size
     private var score = 0
 
+    @SuppressLint("CommitPrefEdits")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_unit1_quiz1_m_c, container, false)
 
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_unit1_quiz1_m_c, container, false)
-
-//        val sharedPref = this.activity?.getSharedPreferences(getString(R.string.preference_key), Context.MODE_PRIVATE)
+        // SharedPreference Object (for storing data locally)
+        val sharedPref = this.activity?.getSharedPreferences(
+            getString(R.string.preference_key), Context.MODE_PRIVATE)
 
         // Initialize questions and set the first question
         initQuestions()
 
+        // Attach quiz variable from quiz 1 layout
         binding.quiz = this
 
         // Set the onClickListener for the nextButton
         binding.nextButton.setOnClickListener @Suppress("UNUSED_ANONYMOUS_PARAMETER")
         { view: View ->
-            handleQuizProper(view)
+            // Handle quiz layout logic and scoring
+            handleQuizProper()
 
+            // Show back button after 1st question
             if (questionIndex > 0) {
                 binding.backButton.visibility = View.VISIBLE
             }
 
+            // Show submit button and hide next button in last question
             if (questionIndex == numQuestions-1) {
                 binding.nextButton.visibility = View.INVISIBLE
                 binding.submitButton.visibility = View.VISIBLE
             }
 
+            // Hide radio group and show edit text in identification part of the quiz
             if (questionIndex > 9) {
                 binding.questionRadioGroup.visibility = View.GONE
                 binding.answerText.visibility = View.VISIBLE
@@ -130,17 +138,20 @@ class Unit1Quiz1MCFragment : Fragment() {
 
         }
 
-        // Set the onClickListener for the nextButton
+        // Set the onClickListener for the backButton
         binding.backButton.setOnClickListener @Suppress("UNUSED_ANONYMOUS_PARAMETER")
         { view: View ->
+            // reset score in current question
+            currentQuestion.isCorrect = false
 
             // move to previous question
-            currentQuestion.isCorrect = false
             questionIndex--
             currentQuestion = questions[questionIndex]
+
             setQuestion()
 
-            // reset fields
+            // Reset fields
+            currentQuestion.isCorrect = false
             binding.invalidateAll()
             binding.questionRadioGroup.clearCheck()
             binding.answerText.text.clear()
@@ -161,20 +172,60 @@ class Unit1Quiz1MCFragment : Fragment() {
                 binding.backButton.visibility = View.INVISIBLE
             }
 
-            if (questionIndex in 0 until numQuestions-1) {
+            // Show next button if not on last question
+            if (questionIndex == numQuestions-2) {
                 binding.nextButton.visibility = View.VISIBLE
             }
+        }
+
+        // Set the onClickListener for the submitButton
+        binding.submitButton.setOnClickListener @Suppress("UNUSED_ANONYMOUS_PARAMETER")
+        { view : View ->
+            // Check last question
+            val answerText = binding.answerText.text.toString()
+            if (answerText.toLowerCase().trim() in currentQuestion.answers) {
+                currentQuestion.isCorrect = true
+            }
+
+            // To make sure button only works once
+            if (score == 0) {
+
+                // Count each correct answer
+                questions.forEach {
+                    if (it.isCorrect){
+                        score++
+                    }
+                }
+
+                // Save score and number of questions in shared preferences
+                with (sharedPref?.edit()) {
+                    this?.putString("quiz1Score", score.toString())
+                    this?.putString("quiz1NumQuestions", numQuestions.toString())
+                    this?.apply()
+                }
+
+                // Navigate to score screen
+                view.findNavController()
+                    .navigate(Unit1Quiz1MCFragmentDirections
+                        .actionUnit1Quiz1MCFragmentToUni1Quiz1ScoreFragment(numQuestions,score))
+            }
+
+
+//            Toast.makeText(context, "Score: $score/$numQuestions", Toast.LENGTH_LONG).show()
         }
 
         return binding.root
     }
 
     // Handles scoring and quiz logic
-    private fun handleQuizProper(view : View) {
+    private fun handleQuizProper() {
+
+        // Multiple choice questions 1-10
         if (questionIndex < 10) {
 
             val checkedId = binding.questionRadioGroup.checkedRadioButtonId
-            // Do nothing if nothing is checked (id == -1)
+
+            // Check if a radio button is selected
             if (-1 != checkedId) {
                 var answerIndex = 0
                 when (checkedId) {
@@ -182,12 +233,13 @@ class Unit1Quiz1MCFragment : Fragment() {
                     R.id.thirdChoiceRadioButton -> answerIndex = 2
                     R.id.fourthChoiceRadioButton -> answerIndex = 3
                 }
+
                 // The first answer in the original question is always the correct one, so if our
                 // answer matches, we have the correct answer.
                 if (answers[answerIndex] == currentQuestion.answers[0]) {
-                    // Add score here
                     currentQuestion.isCorrect = true
                 }
+
                 // Advance to the next question
                 questionIndex++
                 currentQuestion = questions[questionIndex]
@@ -195,22 +247,22 @@ class Unit1Quiz1MCFragment : Fragment() {
                 binding.invalidateAll()
                 binding.questionRadioGroup.clearCheck()
 
-
+                // Show back button
                 binding.backButton.visibility = View.VISIBLE
 
 
-
+            // Prompt user to select an answer
             } else {
                 Toast.makeText(context, "Choose your answer", Toast.LENGTH_SHORT).show()
             }
 
+            // Identification questions 11-15
         } else if (questionIndex in 10 until numQuestions) {
-            Toast.makeText(context, "11-15!!!!! Index: $questionIndex", Toast.LENGTH_SHORT).show()
 
             val answerText = binding.answerText.text
 
             if (answerText.isNotEmpty()) {
-                if (answerText.toString().toLowerCase() in currentQuestion.answers) {
+                if (answerText.toString().toLowerCase().trim() in currentQuestion.answers) {
                     currentQuestion.isCorrect = true
                 }
 
@@ -228,8 +280,8 @@ class Unit1Quiz1MCFragment : Fragment() {
                 Toast.makeText(context,"Enter your answer",Toast.LENGTH_SHORT).show()
             }
         } else {
+            // For error checking only
             Toast.makeText(context, "Error index out of range: $questionIndex", Toast.LENGTH_LONG).show()
-            // Navigate to score fragment
         }
     }
 
