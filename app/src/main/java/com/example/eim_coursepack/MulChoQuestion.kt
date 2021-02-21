f57877f2745d9b69d436c3b37c6d60f2550b49d8
sdlagrosas <sdlagrosas@up.edu.ph>
2021-02-21 16:36:40 +0800

    Implement saving of inputs in quiz fragment

--- app/src/main/java/com/example/eim_coursepack/MulChoQuestion.kt
+++ app/src/main/java/com/example/eim_coursepack/MulChoQuestion.kt
@@ -1,6 +1,14 @@
 package com.example.eim_coursepack
 
-data class Question(
+data class MulChoQuestion(
     val text: String,
-    val answers: List<String>,
-    var isCorrect: Boolean)
+    val answers: MutableList<String>,
+    var isCorrect: Boolean,
+    var clickedIdx : Int,
+    var correctIdx : Int)
+
+data class IdenQuestion (
+    val text: String,
+    val answers: MutableList<String>,
+    var isCorrect: Boolean,
+    var enteredAns : String)
