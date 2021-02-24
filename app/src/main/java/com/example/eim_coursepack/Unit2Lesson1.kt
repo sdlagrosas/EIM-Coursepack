5ef8cb7687d7b2d62d2eb6e1ac4e747be680fe8b
sdlagrosas <sdlagrosas@up.edu.ph>
2021-02-24 21:38:49 +0800

    Add images for unit 2 quiz 1

--- app/src/main/java/com/example/eim_coursepack/Unit2Lesson1.kt
+++ app/src/main/java/com/example/eim_coursepack/Unit2Lesson1.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.eim_coursepack.databinding.FragmentUnit2Lesson1Binding
 
 
 class Unit2Lesson1 : Fragment() {
@@ -13,7 +16,18 @@ class Unit2Lesson1 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_unit2_lesson1, container, false)
+
+        val binding : FragmentUnit2Lesson1Binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_unit2_lesson1, container, false
+        )
+
+        binding.quiz21Button.setOnClickListener { view : View ->
+            view.findNavController()
+                .navigate(Unit2Lesson1Directions
+                    .actionUnit2Lesson1ToUnit2Quiz1Fragment())
+        }
+
+        return binding.root
     }
 
 }
