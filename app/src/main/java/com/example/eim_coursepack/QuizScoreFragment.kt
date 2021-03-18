18ac8bde75633cd34a9b54ac259358de6d50879f
sdlagrosas <sdlagrosas@up.edu.ph>
2021-03-19 01:23:42 +0800

    Apply the allbuttons style to the Return button in quiz score fragment. Route the button's destination to each unit fragments

--- app/src/main/java/com/example/eim_coursepack/QuizScoreFragment.kt
+++ app/src/main/java/com/example/eim_coursepack/QuizScoreFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.example.eim_coursepack.databinding.FragmentQuizScoreBinding
@@ -30,10 +31,23 @@ class QuizScoreFragment : Fragment() {
 
         quizTitle = args.quizTitle
 
+        val unitNo = quizTitle[5]
+
+        val text = quizTitle[5]
+
+        Toast.makeText(context, "$text!", Toast.LENGTH_LONG).show()
+
+        var unitFragment = QuizScoreFragmentDirections.actionQuizScoreFragmentToUnit1Fragment()
+        when(unitNo) {
+            '2' -> unitFragment = QuizScoreFragmentDirections.actionQuizScoreFragmentToUnit2Fragment()
+            '3' -> unitFragment = QuizScoreFragmentDirections.actionQuizScoreFragmentToUnit3Fragment()
+            '4' -> unitFragment = QuizScoreFragmentDirections.actionQuizScoreFragmentToUnit4Fragment()
+            '5' -> unitFragment = QuizScoreFragmentDirections.actionQuizScoreFragmentToUnit5Fragment()
+            '6' -> unitFragment = QuizScoreFragmentDirections.actionQuizScoreFragmentToUnit6Fragment()
+        }
+
         binding.returnButton.setOnClickListener { view : View ->
-            view.findNavController().navigate(
-                QuizScoreFragmentDirections.actionQuizScoreFragmentToUnit1Fragment()
-            )
+            view.findNavController().navigate(unitFragment)
         }
 
         return binding.root
